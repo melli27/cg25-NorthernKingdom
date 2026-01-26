8e952d5a6eab578422add170d42836f5c0e71cce
Tran, Quynh Nhu Jennifer <quynh.tran@tuwien.ac.at>
2026-01-26 07:55:01 +0100

    minor fix

--- src/Model/Model.cpp
+++ src/Model/Model.cpp
@@ -302,6 +302,13 @@ void Model::loadMaterialTextures(aiMesh* mesh, std::vector<std::shared_ptr<Textu
 	aiTextureType types[4] = { aiTextureType_DIFFUSE, aiTextureType_SPECULAR, aiTextureType_NORMALS, aiTextureType_EMISSIVE }; //TODO add more?
 	string typeNames[4] = { "diffuseTexture", "specularTexture", "normalTexture", "emissionTexture" };
 
+	// fallback when normal maps stored in HEIGHT
+	if (material->GetTextureCount(aiTextureType_NORMALS) == 0 &&
+		material->GetTextureCount(aiTextureType_HEIGHT) > 0)
+	{
+		types[2] = aiTextureType_HEIGHT;
+	}
+
 	for (int i = 0; i < 4; i++)
 	{
 		aiTextureType type = types[i];
